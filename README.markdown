3ebb83345aaa57cb8bfc8d51b8cab89994047cc2
Trevor Davis <davist11@gmail.com>
2011-04-02 16:59:22 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,1 @@
+HP Demo Site
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js ie ie6 lte8 lte7"><![endif]--> 
+<!--[if IE 7]><html class="no-js ie ie7 lte8 lte7"><![endif]--> 
+<!--[if IE 8]><html class="no-js ie ie8 lte8"><![endif]--> 
+<!--[if !IE]><!--><html class="no-js"><!--<![endif]-->
+  <head>
+    <title>The Gryffindor News &amp; Notices Board | April Edition</title>
+    <meta charset="utf-8" />
+    <link href="/stylesheets/screen.css" rel="stylesheet" media="screen,projection" />
+    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+  </head>
+  <body>
+    <div id="container">
+      <header>
+        <h1 class="alt">The Gryffindor News &amp; Notices Board</h1>
+        <h2>April Edition</h2>
+      </header>
+      
+      <div class="group primary">
+        <section class="main left">
+          <h2>Gryffindor Pummels Slytherin, 300 to 80!</h2>
+          <p>Lee Jordan reporting, folks! Last week&rsquo;s quidditch match was a bloodbath. Thanks to the deft moves of the Gryffindor team, Slytherin wasn&rsquo;t just beaten, they were destroyed. Check out the action yourself:</p>
+          
+          <iframe title="YouTube video player" width="480" height="293" src="http://www.youtube.com/embed/YJvEzIrt_u8" frameborder="0" allowfullscreen></iframe>
+        </section>
+        
+        <section class="aside right">
+          <h2><span>Read</span> Past Editions</h2>
+          <ul>
+            <li>
+              <a href="#">
+                <h3>March Edition</h3>
+                The OWLs and You. Studying spells for success.
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <h3>February Edition</h3>
+                Common Rules For the Common Room. Or: stop leaving your dirty socks lying about, Fred Weasley.
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <h3>January  Edition</h3>
+                Food Rules For All. Why pumpkin pastries should stay out of your rooms.
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <h3>December  Edition</h3>
+                Winter Holidays! All the ways to get to and from Hogwarts for the holidays that are almost as fast as apparating.
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+      
+      <section class="quidditch">
+        <h2>Quidditch Tryouts Next Week!</h2>
+        <p>Hey everyone, I want to get a headstart on next year. So we&rsquo;ll be holding Quidditch tryouts out on the pitch, next Wednesday at 6pm.</p>
+        <span class="sig">Harry Potter</span>
+        <span class="flyer"></span>
+      </section>
+      
+      <div class="group tertiary">
+        <section class="left">
+          <a href="#">
+            <h2>Slightly Used <span>Cauldron For Sale</span></h2>
+            <p>Only has one crack in it! Selling for 2 Galleons. Please contact Neville Longbottom.</p>
+            <img src="/images/content/cauldron.png" alt="Cauldron" height="155" width="162" />
+          </a>
+        </section>
+        
+        <section class="left">
+          <p>Looking to get some proper training? 
+          Have some free weeknights?</p>
+
+          <p>Can you keep a secret?</p>
+          <span class="alt da">DA</span>
+        </section>
+        
+        <section class="right">
+          <a href="#">
+            <h2>Lost and Found</h2>
+            <img src="/images/content/ink.png" alt="Green ink" height="133" width="121" />
+            <p>One bottle of green ink found in the girls&rsquo; rooms last Thursday. If it&rsquo;s yours, contact Hermione Granger forthwith.</p>
+          </a>
+        </section>
+      </div>
+    
+    </div>
+    <script src="/scripts/jquery.js"></script>
+    <script src="/scripts/global.js"></script>
+  </body>
+</html>
